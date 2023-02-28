176db5c5579639e7ff622bdf10d72b11afde7544
LittleLittleCloud <bigmiao.zhang@gmail.com>
2023-02-27 16:42:30 -0800

    init

---
+++ QANDA.Net/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenAI_API;
+using QANDA.Net;
+
+var api = new OpenAIAPI("your token");
+var rag = new RAG(api);
+var corpus = File.ReadAllLines("corpus.txt");
+await rag.BuildCorpus(corpus);
+var prompt = await rag.CreateSourceEmbedding("Who is John, Is he real", 5);
+var answer = await rag.GenerateOutput(prompt);
+Console.WriteLine(answer);
+
+/*
+output
+load embedding form cache
+FACT: John is a philanthropist and uses his fame to help a variety of charitable causes. He has also been recognized for his work in the field of virtual reality, having been awarded several awards for his work in the industry.
+FACT: John's career began with a minor role in a popular sci-fi TV series. Soon, he was cast in a starring role in a virtual reality film. He was a hit with critics and audiences alike and his career quickly took off. He has since gone on to star in several feature films, as well as several video games and commercials.
+FACT: John Smith is a virtual actor. He was created using advanced artificial intelligence and motion capture technology to bring his performance to life.
+FACT: John loves to explore the world of virtual reality and is always looking for new ways to push the boundaries of technology and performance. He is a frequent speaker at industry conferences and is constantly pushing the boundaries of what is possible with virtual reality.
+FACT: John is always looking for new projects and opportunities. He believes that virtual reality will continue to be an important part of our lives and strives to be at the forefront of its evolution.
+QUESTION: Who is John, Is he real
+
+ANSWER: John Smith is a virtual actor created using advanced artificial intelligence and motion capture technology. He is not a real person, but he is considered a pioneer in the field of virtual reality performance.
+*/
---
+++ QANDA.Net/RAG.cs
@@ -0,0 +1,102 @@
+﻿using OpenAI_API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace QANDA.Net
+{
+    internal class RAG
+    {
+        private Dictionary<string, float[]> embeddings;
+        private OpenAIAPI openApi;
+        private string cachePath = "RAG_cache.json";
+        public RAG(OpenAIAPI openApi)
+        {
+            this.openApi = openApi;
+            if(File.Exists(cachePath))
+            {
+                Console.WriteLine("load embedding form cache");
+                var json = File.ReadAllText(cachePath);
+                var cache = JsonSerializer.Deserialize<Dictionary<string, float[]>>(json);
+                this.embeddings = cache;
+            }
+            else
+            {
+                this.embeddings = new Dictionary<string, float[]>();
+            }
+        }
+
+        public async Task BuildCorpus(IEnumerable<string> corpus, bool byLine = true)
+        {
+            foreach(var corpusItem in corpus)
+            {
+                if (byLine)
+                {
+                    foreach(var line in corpusItem.Split(Environment.NewLine))
+                    {
+                        if (!embeddings.ContainsKey(line))
+                        {
+                            embeddings[line] = await openApi.Embeddings.GetEmbeddingsAsync(line);
+                        }
+                    }
+                }
+                else
+                {
+                    if (!embeddings.ContainsKey(corpusItem))
+                    {
+                        embeddings[corpusItem] = await openApi.Embeddings.GetEmbeddingsAsync(corpusItem);
+                    }
+                }
+            }
+
+            var json = JsonSerializer.Serialize(embeddings);
+            File.WriteAllText(cachePath, json);
+        }
+
+        public async Task<string> CreateSourceEmbedding(string question, int referenceNumber = 10)
+        {
+            float cosineSimilarity(float[] a, float[] b)
+            {
+                int N = 0;
+                N = ((a.Count() < b.Count()) ? a.Count() : b.Count());
+                double dot = 0.0d;
+                double mag1 = 0.0d;
+                double mag2 = 0.0d;
+                for (int n = 0; n < N; n++)
+                {
+                    dot += a[n] * b[n];
+                    mag1 += Math.Pow(a[n], 2);
+                    mag2 += Math.Pow(b[n], 2);
+                }
+
+                return Convert.ToSingle(dot / (Math.Sqrt(mag1) * Math.Sqrt(mag2)));
+            }
+
+            var questionEmbedding = await openApi.Embeddings.GetEmbeddingsAsync(question);
+            var cosineSimilaritys = embeddings.Select(kv => (kv.Key, cosineSimilarity(kv.Value, questionEmbedding)))
+                .OrderByDescending(item => item.Item2)
+                .Take(referenceNumber);
+
+            var sb = new StringBuilder();
+            foreach(var item in cosineSimilaritys)
+            {
+                sb.AppendLine($"FACT: {item.Key}");
+            }
+
+            sb.AppendLine($"QUESTION: {question}");
+
+            Console.WriteLine(sb);
+            return sb.ToString();
+        }
+
+        public async Task<string> GenerateOutput(string prompt)
+        {
+            var result = await openApi.Completions.CreateCompletionAsync(prompt, max_tokens: 512);
+            return result.Completions.First().Text;
+        }
+    }
+}
